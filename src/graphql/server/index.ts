d5eb2520ef6fb719ed169d571f439601be9945b2
Veselin Romić <omegavesko@gmail.com>
2021-09-12 01:18:15 +0200

    feat: GraphQL yak shaving and updateSelf mutation stub

--- src/graphql/server/index.ts
+++ src/graphql/server/index.ts
@@ -1,6 +1,26 @@
-import { ApolloServer } from "apollo-server-micro";
+import { ApolloServer, AuthenticationError } from "apollo-server-micro";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/client";
+import { isProduction } from "../../constants";
 import schema from "./schema";
 
-const server = new ApolloServer({ schema });
+export interface Context {
+  session: Session;
+}
+
+const server = new ApolloServer({
+  schema,
+  context: async ({ req }) => {
+    const session = await getSession({ req });
+
+    if (isProduction && session === null) {
+      throw new AuthenticationError(
+        "You must be logged in to access the GraphQL API."
+      );
+    }
+
+    return { session };
+  },
+});
 
 export default server;
--- src/graphql/server/schema.ts
+++ src/graphql/server/schema.ts
@@ -1,10 +1,19 @@
-import { makeSchema, mutationType, objectType, queryType } from "nexus";
+import {
+  arg,
+  inputObjectType,
+  makeSchema,
+  mutationField,
+  nonNull,
+  objectType,
+} from "nexus";
+import * as path from "path";
+import { isProduction } from "../../constants";
 
 const User = objectType({
   name: "User",
   definition(t) {
     t.id("id");
-    t.string("pronoun");
+    t.nonNull.string("pronoun");
     t.string("avatar");
     t.string("avatarDead");
   },
@@ -14,6 +23,41 @@ const User = objectType({
 
 // const Mutation = mutationType({ definition(t) {} });
 
-const schema = makeSchema({ types: [User] });
+const UpdateSelfInput = inputObjectType({
+  name: "UpdateSelfInput",
+  definition(t) {
+    t.string("pronoun");
+    t.string("avatar");
+    t.string("avatarDead");
+  },
+});
+
+const updateSelf = mutationField("updateSelf", {
+  type: User,
+  args: {
+    input: nonNull(
+      arg({
+        type: UpdateSelfInput,
+      })
+    ),
+  },
+  resolve(_, args, ctx) {
+    const { session } = ctx;
+    console.log(args);
+    console.log(session);
+  },
+});
+
+const schema = makeSchema({
+  types: [User, updateSelf],
+  contextType: {
+    module: path.join(process.cwd(), "./src/graphql/server/index.ts"),
+    export: "Context",
+  },
+  shouldGenerateArtifacts: !isProduction,
+  outputs: {
+    schema: path.join(process.cwd(), "generated/schema.gen.graphql"),
+  },
+});
 
 export default schema;
