570d3269808bfbf044e614cdd6a9a51f7c8a7146
Veselin Romić <omegavesko@gmail.com>
2021-09-12 04:30:39 +0200

    fix: ID should be non-nullable

--- src/graphql/server/types.ts
+++ src/graphql/server/types.ts
@@ -3,7 +3,7 @@ import { objectType } from "nexus";
 export const User = objectType({
   name: "User",
   definition(t) {
-    t.id("id");
+    t.nonNull.id("id");
     t.nonNull.string("pronoun");
     t.string("avatar");
     t.string("avatarDead");
